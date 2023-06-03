cc8ee418ed43c9752440d77477fe1b7afc5e0655
andrewzhivolup <ferari1190@mail.ru>
2023-06-03 19:27:44 +0300

    add className function

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -14,7 +14,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         output: {
             filename: "[name].[contenthash].js",
             path: paths.build,
-            clean: true
+            clean: true,
         },
         plugins: buildPlugins(options),
         module: {
