346dfe24c7e1fb2dc345cb12c2aa31c69a0f0fe6
andrewzhivolup <ferari1190@mail.ru>
2023-06-21 20:42:40 +0300

    fix navbar error, remove screenshot testing from github actions

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -27,7 +27,9 @@ export const Navbar = ({ className }: NavbarProps) => {
                 {t('Войти')}
             </Button>
             <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid commodi consequatur eligendi impedit incidunt
+                necessitatibus possimus quis saepe sunt totam.
             </Modal>
         </div>
     );
